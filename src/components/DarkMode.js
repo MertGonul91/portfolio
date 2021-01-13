f20f43e912cc382227e3b5e6643e485feeacb6e5
MertGonul91 <mertgonul91@gmail.com>
2021-01-13 18:40:19 +0100

    Adjustments on Responsive Layout

--- src/components/DarkMode.js
+++ src/components/DarkMode.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import {
-  Button,
-  IconButton,
-  useColorMode,
-  Text,
-  useMediaQuery,
-} from '@chakra-ui/react';
+import { IconButton, useColorMode } from '@chakra-ui/react';
 import { BsMoon, BsSun } from 'react-icons/bs';
 
 const DarkMode = () => {
   const { colorMode, toggleColorMode } = useColorMode();
   return (
     <>
-      <IconButton onClick={toggleColorMode} size='md'>
+      <IconButton
+        onClick={toggleColorMode}
+        size='md'
+        _hover={{
+          bgGradient: 'linear(to-r, red.500, yellow.500)',
+        }}
+      >
         {colorMode === 'light' ? <BsMoon /> : <BsSun />}
       </IconButton>
     </>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,53 +1,28 @@
 import React from 'react';
 import DarkMode from './DarkMode';
-import { Box, Flex, Spacer, Link, useMediaQuery, Text } from '@chakra-ui/react';
+import { Box, Flex, Spacer, Link } from '@chakra-ui/react';
 import { Link as RouterLink } from 'react-router-dom';
 
 const Header = () => {
-  const [mobile] = useMediaQuery('(max-width: 450px)');
   return (
     <>
-      {mobile ? (
-        <Flex
-          flexWrap='wrap'
-          alignItems='center'
-          justifyContent='space-between'
-        >
-          <Box p='0' m='2px'>
-            <Link as={RouterLink} to='/'>
-              <Text fontSize='xs'>home.</Text>
-            </Link>
-          </Box>
+      <Flex justifyContent='space-between'>
+        <Box p='10'>
+          <Link as={RouterLink} to='/'>
+            home.
+          </Link>
+        </Box>
 
-          <Box p='0' m='2px'>
-            <DarkMode />
-          </Box>
+        <Box p='10'>
+          <DarkMode />
+        </Box>
 
-          <Box p='0' m='2px'>
-            <Link as={RouterLink} to='/projects'>
-              <Text fontSize='xs'>webdev projects.</Text>
-            </Link>
-          </Box>
-        </Flex>
-      ) : (
-        <Flex>
-          <Box p='10'>
-            <Link as={RouterLink} to='/'>
-              home.
-            </Link>
-          </Box>
-          <Spacer />
-          <Box p='10'>
-            <DarkMode />
-          </Box>
-          <Spacer />
-          <Box p='10'>
-            <Link as={RouterLink} to='/projects'>
-              webdev projects.
-            </Link>
-          </Box>
-        </Flex>
-      )}
+        <Box p='10'>
+          <Link as={RouterLink} to='/projects'>
+            webdev projects.
+          </Link>
+        </Box>
+      </Flex>
     </>
   );
 };
