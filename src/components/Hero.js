9e448b4109aad44dc4075c3d316e8910113a2602
MertGonul91 <mertgonul91@gmail.com>
2021-01-15 17:53:38 +0100

    Fixed imports

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,13 +1,8 @@
 import React from 'react';
-import { Flex, useMediaQuery, Container } from '@chakra-ui/react';
+import { Flex, Container } from '@chakra-ui/react';
 import About from './About';
 
 const Hero = () => {
-  const [desktop] = useMediaQuery('(min-width: 1280px)');
-  const [mobile] = useMediaQuery('(max-width: 450px)');
-
-  const about = "I'm Mert Gönül, a Full Stack Developer and an Architect.";
-
   return (
     <>
       <Container maxW='60vw' maxH='90vh'>
